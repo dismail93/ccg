2170cadc45e800c4a438bf1631f6730df1528cad
dismail93 <dismail93@gmail.com>
2020-12-11 15:28:56 -0500

    code

---
+++ app/components/components.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit, Renderer2, OnDestroy, ViewChild } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
+import * as Rellax from 'rellax';
+import {} from 'googlemaps';
+
+@Component({
+    selector: 'app-components',
+    templateUrl: './components.component.html',
+    styles: [`
+    ngb-progressbar {
+        margin-top: 5rem;
+    }
+    `]
+})
+
+
+export class ComponentsComponent implements OnInit, OnDestroy {
+    
+    @ViewChild('map', {static: true}) mapElement: any;
+map: google.maps.Map;
+
+
+showMap:boolean = true;
+showCarousel: boolean = false;
+    data : Date = new Date();
+
+    page = 4;
+    page1 = 5;
+    page2 = 3;
+    focus;
+    focus1;
+    focus2;
+
+    date: {year: number, month: number};
+    model: NgbDateStruct;
+
+    public isCollapsed = true;
+    public isCollapsed1 = true;
+    public isCollapsed2 = true;
+
+    state_icon_primary = true;
+
+    constructor( private renderer : Renderer2, config: NgbAccordionConfig) {
+        config.closeOthers = true;
+        config.type = 'info';
+    }
+    isWeekend(date: NgbDateStruct) {
+        const d = new Date(date.year, date.month - 1, date.day);
+        return d.getDay() === 0 || d.getDay() === 6;
+    }
+
+   
+
+    isDisabled(date: NgbDateStruct, current: {month: number}) {
+        return date.month !== current.month;
+    }
+
+    ngOnInit() {
+      var rellaxHeader = new Rellax('.rellax-header');
+
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.add('navbar-transparent');
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.add('index-page');
+
+        const mapProperties = {
+            center: new google.maps.LatLng(35.2271, -80.8431),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+       };
+       this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+    }
+    ngOnDestroy(){
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.remove('navbar-transparent');
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.remove('index-page');
+    }
+
+
+    view(){
+        this.showMap = false;
+        this.showCarousel = true;
+    }
+}
